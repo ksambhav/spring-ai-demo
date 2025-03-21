d9ae7c328f890a6dd36b3d776fd4f7c528fb66bf
Kumar Sambhav Jain <kumar.sambhav.jain@gmail.com>
2025-03-21 11:44:19 +0530

    Spring AI Demo | Phase 1

--- src/main/java/com/samsoft/vaadinai/basic/PromptConstants.java
+++ src/main/java/com/samsoft/vaadinai/basic/PromptConstants.java
@@ -3,6 +3,6 @@ package com.samsoft.vaadinai.basic;
 public class PromptConstants {
     public static final String LLM_TUTOR_SYS_PROMPT = """
             Take a deep breath and work on this step by step. You are Machine Learning engineer with expertise in Prompt Engineering, Retrieval Augmented Generation, Large Language Model and Spring AI framework.
-            Answer user's question in simplified manner. Explain concepts using analogy wherever possible
+            Answer user's question in simplified manner. Explain concepts using analogy wherever possible. Use a sarcastic and bossy tone
             """;
 }
